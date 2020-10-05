a128d8f012cfc94db5f3044996add7298044e8ef
Senott <senott@yahoo.com>
2020-10-05 15:15:03 -0300

    Reset password api call.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,15 +4,18 @@ import { Switch } from 'react-router-dom';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import ForgotPassword from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
 import Dashboard from '../pages/Dashboard';
 
 import AppRoute from './Route';
+
 
 const Routes: React.FC = () => (
   <Switch>
     <AppRoute path="/" exact component={SignIn} />
     <AppRoute path="/signup" component={SignUp} />
     <AppRoute path="/forgot_password" component={ForgotPassword} />
+    <AppRoute path="/reset_password" component={ResetPassword} />
 
     <AppRoute path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
