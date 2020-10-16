25361cc581c3dd69aee5b005736425161dbf4a5a
Senott <senott@yahoo.com>
2020-10-16 14:18:16 -0300

    Button component tests.

---
+++ src/__tests__/components/Button.spec.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import Button from '../../components/Button';
+
+describe('Button Component', () => {
+  it('should render button', () => {
+    const { getByText } = render(<Button type="button">Ok</Button>);
+
+    const buttonElement = getByText('Ok');
+
+    expect(buttonElement).toBeTruthy();
+  });
+
+  it('should chenge button text when clicked', () => {
+    const { getByText } = render(
+      <Button type="button" loading>
+        Ok
+      </Button>,
+    );
+
+    const buttonElement = getByText('Carregando...');
+
+    expect(buttonElement).toBeTruthy();
+  });
+});
